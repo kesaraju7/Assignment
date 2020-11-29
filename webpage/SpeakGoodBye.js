f5b08f0ecc4ecda18126eea786af15f7cd6e111f
kesaraju7 <swethakesaraju77@gmail.com>
2020-11-29 16:36:01 +0530

    module-4

---
+++ webpage/SpeakGoodBye.js
@@ -0,0 +1,6 @@
+var byeSpeaker={};
+
+var speakWord="Good Bye";
+byeSpeaker.speak=function speak (input){
+ console.log(speakWord+" "+input);
+}
---
+++ webpage/SpeakHello.js
@@ -0,0 +1,10 @@
+var helloSpeaker = {};
+
+helloSpeaker.speak= function(input){
+    var speakWord ='Hello';
+    console.log(speakWord+" "+input);
+}
+
+
+
+
